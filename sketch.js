99aad78595c50a1664a31ca264c6baf764a32c45
behappy04 <72173903+behappy04@users.noreply.github.com>
2020-12-09 20:03:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var wall,car,thickness;
+var speed,weight,bullet;
+function setup() {
+  createCanvas(1600,400);
+
+  bullet = createSprite(50,200,50,50);
+  speed = random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet.velocityX = speed
+
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+
+  car.velocityX = speed;
+  car.shapeColor = color(255);
+}
+
+function draw() {
+  background(0);  
+
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX = 0
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+    if(damage>10){
+      bullet.velocityX = 0;
+      wall.shapeColor = color(255,0,0);
+    }
+
+    if(damage<10){
+      wall.shapeColor = color(0,255,0);
+      bullet.velocityX = 0;
+    }
+  }
+  drawSprites();
+}
+
+function hasCollided(lbullet, lwall){   
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallLeftEdge=lwall.x;
+  if (bulletRightEdge>+wallLeftEdge){
+    return true
+  }
+  return false;
+}
